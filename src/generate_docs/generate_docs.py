371796224d75e0f41f700803a221d351d6ec18f9
Tim Golden <tim.golden@global.com>
2024-08-07 22:16:47 +0100

    Working version

--- src/generate_docs/generate_docs.py
+++ src/generate_docs/generate_docs.py
@@ -2,39 +2,153 @@
 """
 import os, sys
 import argparse
+from collections import namedtuple, defaultdict
 import logging
 from pathlib import Path
+from pprint import pprint
+import re
+import shutil
 import subprocess
+import tempfile
 
-HERE = Path(__file__)
+import openpyxl
+
+HERE = Path(__file__).parent
+NAME = HERE.stem
 print(HERE)
+DBT_LAYOUT = HERE / ".." / "graphdoc"
+print(DBT_LAYOUT)
 
-logger = logging.getLogger(os.path.basename(__file__))
+logger = logging.getLogger(NAME)
 logger.setLevel(logging.DEBUG)
 
 stdout_handler = logging.StreamHandler()
-stdout_handler.setLevel(logging.INFO)
+stdout_handler.setLevel(logging.DEBUG)
 logger.addHandler(stdout_handler)
 
-log_file_path = "dump_snowflake_databases.log"
+log_file_path = f"{NAME}.log"
 log_format = "%(name)s - %(asctime)s - %(levelname)s - %(message)s"
 file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
 file_handler.setFormatter(logging.Formatter(log_format))
 file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
-error_file_path = "dump_snowflake_databases.errors.log"
+error_file_path = f"{NAME}.errors.log"
 error_handler = logging.FileHandler(error_file_path)
 error_handler.setFormatter(logging.Formatter(log_format))
 error_handler.setLevel(logging.WARN)
 logger.addHandler(error_handler)
 
+def to_identifier(s):
+    """Turn a string into a valid identifier
+
+    Replace any non-alphanumerics by underscores
+    """
+    return "_".join(i for i in re.findall(r"\w*", s) if i)
+
+def data_from_spreadsheet(xlsx_filepath, sheet_name):
+    xlsx_filepath = Path(xlsx_filepath)
+    wb = openpyxl.load_workbook(
+        xlsx_filepath,
+        data_only=True, read_only=True
+    )
+    ws = wb[sheet_name]
+    irows = ws.iter_rows()
+    headers = [to_identifier(c.value) for c in next(irows)]
+    tuple_name = to_identifier(xlsx_filepath.stem + " " + sheet_name)
+    datatuple = namedtuple(tuple_name, headers)
+    for row in irows:
+        yield datatuple._make(c.value for c in row)
+
+def copy_dbt_layout(target_dirpath, dirname):
+    target_dirpath = Path(target_dirpath or tempfile.mkdtemp())
+    dbt_dirpath = target_dirpath / dirname
+    logger.debug("Copying dbt layout to %s", dbt_dirpath)
+    shutil.copytree(DBT_LAYOUT, dbt_dirpath, dirs_exist_ok=True)
+    return dbt_dirpath
+
+def get_objects_from_xlsx(xlsx_filepath):
+    #
+    # Read dependencies first because they're needed
+    # when building the objects
+    #
+    dependencies = {}
+    for dep in data_from_spreadsheet(xlsx_filepath, "Dependencies"):
+        dependencies.setdefault(dep.Object, []).append(dep.Depends_On)
+
+    objects = {}
+    for object in data_from_spreadsheet(xlsx_filepath, "Objects"):
+        tags = set(t.strip() for t in (object.Tags or "").split(",") if t.strip())
+        objects[object.Object] = dict(
+            group=object.Group,
+            description=object.Description,
+            depends_on=dependencies.get(object.Object, []),
+            tags=tags
+        )
+
+    return objects
+
+def model_contents(object):
+    for dep in object.get('depends_on', []):
+        yield "-- depends on {{ref('%s')}}" % dep
+    tags = object.get("tags", []) or []
+    if tags:
+        yield "{{config(tags=[%s])}}" % ",".join("'%s'" % t for t in tags)
+    yield "SELECT 1 AS x"
+
+def write_one_model(model_filepath, object):
+    """
+    -- depends on {{ref('dbo.RAORDERS')}}
+    -- depends on {{ref('dbo.RAORDERSPOTS')}}
+    -- depends on {{ref('dbo.RASCHEDULEDSPOTS')}}
+    -- depends on {{ref('dbo.RASTATIONS')}}
+    -- depends on {{ref('dbo.RASPOTTYPES')}}
+    -- depends on {{ref('dbo.RADATASOURCES')}}
+    -- depends on {{ref('dbo.RACLIENTS')}}
+    -- depends on {{ref('dbo.RAORDERPRICINGLINES')}}
+    -- depends on {{ref('dbo.RAAIRTIME')}}
+    -- depends on {{ref('dbo.RAOFFAIRACTIVITIES')}}
+    --{{config(tags=['DWH_Daybook'])}}
+    select 1 as x
+    """
+    with model_filepath.open("w") as f:
+        for content in model_contents(object):
+            f.write(content + "\n")
+
+def write_models(dbt_dirpath, objects):
+    models_dirpath = dbt_dirpath / "models"
+    for object_name, object in objects.items():
+        group = (object.get('group') or "").strip() or "database"
+        model_dirpath = models_dirpath / group
+        model_dirpath.mkdir(exist_ok=True)
+        model_filepath = model_dirpath / (object_name + ".sql")
+        print("Model:", model_filepath)
+        write_one_model(model_filepath, object)
+
+def dbt_generate_docs_(dbt_dirpath):
+    subprocess.run(["dbt", "docs", "generate", "--project-dir=%s" % dbt_dirpath, "--profiles-dir=%s" % dbt_dirpath], check=True)
+    subprocess.run(["dbt", "docs", "serve", "--project-dir=%s" % dbt_dirpath, "--profiles-dir=%s" % dbt_dirpath], check=True)
+
 def run(args):
-    print(args.filepath)
+    """Merge command-line args with defaults and run core functions
+    """
+    xlsx_filepath = Path(args.xlsx_filepath)
+    #
+    # Extract the objects and their dependencies from the specified spreadsheet
+    # Copy the template DBT project to a temp or other directory
+    # Write the models corresponding to the objects into the dbt project
+    #
+    objects = get_objects_from_xlsx(xlsx_filepath)
+    dbt_dirpath = copy_dbt_layout(args.target_dirpath, xlsx_filepath.stem)
+    write_models(dbt_dirpath, objects)
+    dbt_generate_docs_(dbt_dirpath)
 
 def command_line():
+    """Do basic command-line arg parsing and hand off to `run`
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath", help="A spreadsheet containing the dependencies")
+    parser.add_argument("xlsx_filepath", help="A spreadsheet containing the dependencies")
+    parser.add_argument("--target-dirpath", help="Directory to build docs in (defaults to a random temp directory)")
     args = parser.parse_args()
     run(args)
 
